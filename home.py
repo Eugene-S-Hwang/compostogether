812b81eaf0eb3cefdf0e1c820a32515ca53073e3
Eugene Hwang <eugenehwang@Eugenes-MBP.fios-router.home>
2024-06-27 02:02:30 -0400

    Testing

--- home.py
+++ home.py
@@ -6,30 +6,30 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Function to load data from Google Sheets
 def load_data():
-    # # Define the scope
-    # scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    # Define the scope
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
-    # # Add credentials
-    # creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-    # client = gspread.authorize(creds)
+    # Add credentials
+    creds = ServiceAccountCredentials.from_json_keyfile_name('compostogether-data-c8dd8dad8d01.json', scope)
+    client = gspread.authorize(creds)
 
-    # # Open the Google Sheet
-    # sheet = client.open("Composting Weekly Log").sheet1
+    # Open the Google Sheet
+    sheet = client.open("Composting Weekly Log").sheet1
 
-    # # Get all records
-    # data = sheet.get_all_records()
-    # return pd.DataFrame(data)
-    data =  pd.read_csv("Testing_Activity.csv")
-    return data
+    # Get all records
+    data = sheet.get_all_records()
+    return pd.DataFrame(data)
+    # data =  pd.read_csv("Testing_Activity.csv")
+    # return data
 
 # Load data
 st.title("Community Composting Program Weekly Log")
-# st.write("Displaying data from Google Sheets")
+st.write("Displaying data from Google Sheets")
 
 data = load_data()
 
 # Display data
-# st.write(data)
+st.write(data)
 
 # Plotly chart
 if not data.empty:
